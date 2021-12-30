4de10d6daf3eebf7edb0d49dfb809f2aa16b3470
Rowan Chatterjee <rowanchat@gmail.com>
2021-12-30 18:08:58 -0500

    Ability

--- frontend/components/AtAGlance.js
+++ frontend/components/AtAGlance.js
@@ -35,7 +35,7 @@ const AtAGlance = props => {
 							<h2>New Members Added<br/>This Year</h2>
 						</Col>
 						<Col xs={24} sm={8}>
-							<h1 style={{color: 'rgb(0, 85, 150)', fontSize: '3em'}}>$<CountUp start={0} end={10658} duration={6} /></h1>
+							<h1 style={{color: 'rgb(0, 85, 150)', fontSize: '3em'}}>$<CountUp start={0} end={13397} duration={6} /></h1>
 							<h2>Ability Experience<br/>Dollars Raised</h2>
 						</Col>
 					</Col>
